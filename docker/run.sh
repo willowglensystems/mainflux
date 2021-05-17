d7c36f05f3ec82252d4e278eb381b6ccae7970e6
Sara <sara.gholami@willowglensystems.com>
2021-05-17 10:41:51 -0600

    Added a separate docker-compose for NATS. Changed the constants in queue-configuration.go.

--- docker/run.sh
+++ docker/run.sh
@@ -10,6 +10,9 @@ MF_QUEUE_SYSTEM=$(read_var MF_QUEUE_SYSTEM .env)
 
 if [ $MF_QUEUE_SYSTEM = "rabbitmq" ]; then
     docker-compose -f rabbitmq.yml up
+elif [ $MF_QUEUE_SYSTEM = "nats" ]; then
+    docker-compose -f nats.yml up
 else
+    # The default queue system is NATS
     docker-compose -f docker-compose.yml up
 fi
